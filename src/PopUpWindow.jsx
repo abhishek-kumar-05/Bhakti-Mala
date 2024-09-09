1d2126fdd0d17ba87d19b980d645a540effd0f31
abhishek-kumar-05 <kumarabhi0513@gmail.com>
2024-09-10 00:06:56 +0530

    create popUp window component

---
+++ src/PopUpWindow.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PopUpWindow = () => {
+  return (
+    <div id="popup">
+      <div className="popup-container">
+        <h1>Review</h1>
+      </div>
+    </div>
+  )
+}
+
+export default PopUpWindow
